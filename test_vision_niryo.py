d47fbbfbb04bcd65c49dd68b9b2162fe8c899707
Ismail Hamrouni <ismail_hamrouni@yahoo.fr>
2024-02-14 21:41:58 +0100

    add save workspace with 4 points

--- test_vision_niryo.py
+++ test_vision_niryo.py
@@ -5,7 +5,12 @@ robot = NiryoRobot("192.168.0.21")
 robot.calibrate_auto()
 
 print("Start Vision")
+point1 = (0.29,-0.06,0.117)
+point2 = (0.146,0.073,0.119)
+point3 = (0.147,0.074,0.118)
+point4 = (0.289,0.076,0.115)
 
+robot.save_workspace_from_points("wp_test_save",point1,point2,point3,point4)
 list_workspaces = robot.get_workspace_list()
 print(list_workspaces)
 
